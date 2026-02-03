28e049ac581a7972ed4762a77537711db74c7215
LinQ <linqiujing77@gmail.com>
2026-02-03 16:04:04 +0900

    README update

---
+++ js/shrines.js
@@ -0,0 +1,44 @@
+const shrines = {
+  "spots": [
+    {
+      "spotID": "1",
+      "zodiacID": "1",
+      "spot": "大豊神社",
+      "spotHiragana": "おおとよじんじゃ",
+      "addr": "京都府京都市左京区鹿ケ谷宮ノ前町1",
+      "spotCatch": "狛ねずみが迎える哲学の道の社",
+      "spotDesc": "平安時代に創建された歴史ある神社。鹿ケ谷の静かな地に鎮座し、四季折々の自然が美しい。豊穣や学問の神として知られる大己貴命を祀る。境内は落ち着いた雰囲気で、地元の人々に親しまれている。",
+      "spotSite": "https://ootoyojinja.jp/"
+    },
+    {
+      "spotID": "2",
+      "zodiacID": "1",
+      "spot": "戸部杉山神社",
+      "spotHiragana": "とべすぎやまじんじゃ",
+      "addr": "神奈川県横浜市西区中央1-13-1",
+      "spotCatch": "横浜最古の歴史ある神社",
+      "spotDesc": "横浜の古社で、源頼朝が創建したと伝わる。杉山明神を祀り、厄除けや健康祈願で信仰を集める。都会の中の静寂な空間として、訪れる人を癒す。春には桜が咲き、見事な景観が楽しめる。",
+      "spotSite": "https://www.kanagawa-jinja.or.jp/shrine/1204011-000/"
+    },
+    {
+      "spotID": "3",
+      "zodiacID": "1",
+      "spot": "大国主神社",
+      "spotHiragana": "しきつまつのみや",
+      "addr": "大阪府大阪市浪速区敷津西1-2-12",
+      "spotCatch": "米俵を抱える狛ねずみの社",
+      "spotDesc": "大国主大神を祀る神社で、縁結びや商売繁盛のご利益で知られる。大阪の喧騒から離れた場所にあり、参拝者に穏やかなひと時を提供。シンプルながらも温かみのある境内が特徴。",
+      "spotSite": "https://www.city.osaka.lg.jp/naniwa/page/0000001078.html"
+    },
+    {
+      "spotID": "4",
+      "zodiacID": "1",
+      "spot": "多賀神社",
+      "spotHiragana": "たがじんじゃ",
+      "addr": "青森県弘前市桜庭字外山948",
+      "spotCatch": "津軽一代様のひとつ",
+      "spotDesc": "弘前の山間に佇む神社で、多賀大神を祀る。清水観音堂として知られ、子育てや長寿の祈願に訪れる人が多い。自然に囲まれた静寂な環境で、心が洗われるような場所。",
+      "spotSite": "https://www.trip-tsugaru.com/special/ichidaisama/"
+    },
+  ]
+};
